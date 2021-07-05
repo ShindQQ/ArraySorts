570c297e2f285f065c13b4937695fa3448347312
Fedorov Denys <denis110402@gmail.com>
2021-07-05 11:15:53 +0300

    Update

--- Sorts_faster_than_ONlogN/main.cpp
+++ Sorts_faster_than_ONlogN/main.cpp
@@ -17,6 +17,11 @@
 
 void swap(int& arg1, int& arg2);
 
+// CountSort
+int FindSmallest(int* arr, int size);
+int FindBiggest(int* arr, int size);
+int* CountSort(int* arr, int size, int biggest, int smallest);
+
 int main()
 {
     FILE* fp;
@@ -44,7 +49,7 @@ int main()
 
     fclose(fp);
 
-
+    arr = CountSort(arr, CONST_10k, FindBiggest(arr, CONST_10k), FindSmallest(arr, CONST_10k));
 
     for (int i = 0; i < CONST_10k; i++)
     {
@@ -60,4 +65,56 @@ void swap(int& arg1, int& arg2)
     int tmp = arg1;
     arg1 = arg2;
     arg2 = tmp;
+}
+
+int FindSmallest(int* arr, int size)
+{
+    int smallest = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (smallest > arr[i])
+        {
+            smallest = arr[i];
+        }
+    }
+    return smallest;
+}
+
+int FindBiggest(int* arr, int size)
+{
+    int biggest = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (biggest < arr[i])
+        {
+            biggest = arr[i];
+        }
+    }
+    return biggest;
+}
+
+int* CountSort(int* arr, int size, int biggest, int smallest)
+{
+    int* tmp_arr = NULL;
+    int iterator = 0;
+    int tmp_size = biggest + abs(smallest) + 1;
+    if (!(tmp_arr = (int*)calloc(tmp_size, sizeof(int))))
+    {
+        printf("Not enough memory for array! LINE: %d", __LINE__);
+        exit(1);
+    }
+    for (int i = 0; i < size; i++)
+    {
+        tmp_arr[arr[i] + abs(smallest)]++;
+    }
+    for (int i = 0; i < tmp_size; i++)
+    {
+        for (int j = 0; j < tmp_arr[i]; j++)
+        {
+            arr[iterator] = smallest + i;
+            iterator++;
+        }
+    }
+    free(tmp_arr);
+    return arr;
 }
